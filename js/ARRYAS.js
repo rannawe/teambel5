cc53c861331d1de28a0968e293a2a00505c95ee3
rannawe <rannawe.x64@gmail.com>
2021-12-08 11:36:34 +0200

    create teambel with js

---
+++ js/ARRYAS.js
@@ -0,0 +1,23 @@
+let zero = 0;
+let counter = 3;
+let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal","Ameer"];
+
+//one//
+/*let last = my.pop();
+my.pop();
+console.log(my.reverse());   //[ "Osama","Elham","Mazero","Ahmed" ]
+*/
+//two//
+//console.log(my.slice()); //["Elham","Mazero"]
+
+//three//
+/*zero  = "Elzero";
+console.log(zero);  //"Elzero"
+*/
+//four//
+
+console.log(("my[1][4],my[1][5]").join(""));   //"rO"
+
+
+
+
